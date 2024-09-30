205ff93c3ec3eca16a5f5720f85ce0e087b608f4
RafayethRafi <rhrafistudent@gmail.com>
2024-10-01 03:48:20 +0600

    Refactor image fetching in HomePage component

--- app/page.jsx
+++ app/page.jsx
@@ -1,35 +1,13 @@
 export const runtime = "edge";
 
+import { getImages } from '@/lib/imageService';
 import HeroSection from '@/components/HeroSection';
 import SportsSection from '@/components/SportsSection';
 import ReviewSection from '@/components/ReviewSection';
 import Footer from '@/components/Footer';
 
-async function getImage(endpoint) {
-  const api = process.env.NEXT_PUBLIC_API_URL;
-  try {
-    const response = await fetch(`${api}${endpoint}`, { cache: 'no-store' });
-    if (response.ok) {
-      return await response.json();
-    }
-    return null;
-  } catch (error) {
-    console.error(`Failed to fetch image from ${endpoint}:`, error);
-    return null;
-  }
-}
-
-async function getImages() {
-  const [hero, cricket, football] = await Promise.all([
-    getImage('/users/main_background_image'),
-    getImage('/users/cricket_background_image'),
-    getImage('/users/football_background_image')
-  ]);
-
-  return { hero, cricket, football };
-}
-
 export default async function HomePage() {
+  console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
   const images = await getImages();
 
   return (
